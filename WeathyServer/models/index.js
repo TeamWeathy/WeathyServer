f9b3a6b7f9c2cec4cb3e122dfe396436cf14a18d
seonuk <sunwook317@hanmail.net>
2021-01-05 00:59:29 +0900

    수정

--- WeathyServer/models/index.js
+++ WeathyServer/models/index.js
@@ -1,42 +1,22 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/database.json')[env];
-const batch_DB = {};
+const db = {};
 
-let batchSequelize;
+let sequelize;
 
 if (config.use_env_variable) {
-    batchSequelize = new Sequelize(
-        process.env[config.use_env_variable],
-        config['batch']
-    );
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    batchSequelize = new Sequelize(
-        config['batch'].database,
-        config['batch'].username,
-        config['batch'].password,
-        config['batch']
+    sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config
     );
 }
 
-batch_DB.sequelize = batchSequelize;
-batch_DB.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-batch_DB.HourlyWeather = require('./hourlyWeather')(batchSequelize, Sequelize);
-batch_DB.DailyWeather = require('./dailyWeather')(batchSequelize, Sequelize);
-batch_DB.Location = require('./location')(batchSequelize, Sequelize);
-batch_DB.Climate = require('./climate')(batchSequelize, Sequelize);
-
-batch_DB.Location.hasMany(batch_DB.HourlyWeather, { onDelete: 'cascade' });
-batch_DB.HourlyWeather.belongsTo(batch_DB.Location);
-
-batch_DB.Location.hasMany(batch_DB.HourlyWeather, { onDelete: 'cascade' });
-batch_DB.DailyWeather.belongsTo(batch_DB.Location);
-
-batch_DB.Climate.hasMany(batch_DB.DailyWeather, { onDelete: 'cascade' });
-batch_DB.DailyWeather.belongsTo(batch_DB.Climate);
-
-batch_DB.Climate.hasMany(batch_DB.HourlyWeather, { onDelete: 'cascade' });
-batch_DB.HourlyWeather.belongsTo(batch_DB.Climate);
-
-module.exports = batch_DB;
+module.exports = db;

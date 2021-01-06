8eb6a6090a266dee4b95910ae5f26610101655d1
seonuk <sunwook317@hanmail.net>
2021-01-07 00:09:31 +0900

    #7 add Token model define

--- WeathyServer/models/weathy.js
+++ WeathyServer/models/weathy.js
@@ -1,25 +1,7 @@
-const { User, WeathyClothes } = require('../models');
-
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define(
         'Weathies',
         {
-            user_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                reference: {
-                    model: User,
-                    key: 'id'
-                }
-            },
-            dailyWeather_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                reference: {
-                    model: WeathyClothes,
-                    key: 'id'
-                }
-            },
             emoji_id: {
                 type: DataTypes.INTEGER,
                 allowNull: false
--- WeathyServer/models/weathyClothes.js
+++ WeathyServer/models/weathyClothes.js
@@ -1,30 +1,10 @@
-const { Weathy, Clothes } = require('../models');
-
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define(
         'WeathyClothes',
-        {
-            weathy_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                reference: {
-                    model: Weathy,
-                    key: 'id'
-                }
-            },
-            clothes_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                reference: {
-                    model: Clothes,
-                    key: 'id'
-                }
-            }
-        },
+        {},
         {
             underscored: true,
-            freezeTableName: true,
-            paranoid: true
+            freezeTableName: true
         }
     );
 };

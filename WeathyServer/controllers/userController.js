9965645c53febc8194330d418fb2b43236296d6e
yxxshin <samshin3910@snu.ac.kr>
2021-01-13 06:37:28 +0900

    Updated userController

--- WeathyServer/controllers/userController.js
+++ WeathyServer/controllers/userController.js
@@ -48,14 +48,13 @@ module.exports = {
                 userId,
                 nickname
             );
-            const token = reqToken;
 
             return res.status(statusCode.OK).json({
                 user: {
                     id: user.id,
                     nickname: user.nickname
                 },
-                token: token,
+                token: reqToken,
                 message: '유저 닉네임 변경 성공'
             });
         } catch (error) {

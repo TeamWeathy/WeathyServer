774887d58b9c0f9b37518b6352bc4f22d330b496
seonuk <sunwook317@hanmail.net>
2021-01-14 21:22:54 +0900

    [HOTFIX] add transaction in token middleware

--- WeathyServer/modules/tokenMiddleware.js
+++ WeathyServer/modules/tokenMiddleware.js
@@ -1,7 +1,7 @@
 const createError = require('http-errors');
 
 const dayjs = require('dayjs');
-const { Token } = require('../models');
+const { Token, sequelize } = require('../models');
 const sc = require('./statusCode');
 const exception = require('./exception');
 const { generateToken } = require('../utils/tokenUtils');
@@ -10,12 +10,12 @@ const TOKEN_EXPIRES_IN_HOURS = 1; // 토큰 유효 기간 (1시간)
 
 const checkTokenExpired = async (token) => {
     // token이 expired 되었는지 확인
-    
+
     const updatedTime = dayjs(token.updatedAt);
     const expirationTime = updatedTime.add(TOKEN_EXPIRES_IN_HOURS, 'h');
 
     const now = dayjs(new Date());
-    
+
     if (now.isBefore(expirationTime)) {
         return false;
     } else {
@@ -36,23 +36,36 @@ const getUserIdByToken = async (token) => {
 };
 
 const refreshTokenTimeOfUser = async (user_id) => {
-    // Sequalizer에서 Token (시간만) 업데이트 하는 코드
-    const userToken = await Token.findOne({ where: {user_id: user_id }});
-    const token = userToken.token;
-    const fakeToken = generateToken(user_id);
-    await Token.update({ token: fakeToken }, { where: { user_id: user_id } });
-    await Token.update({ token: token }, { where: { user_id: user_id }});
-}
+    try {
+        const transaction = await sequelize.transaction();
+        // Sequalizer에서 Token (시간만) 업데이트 하는 코드
+        const userToken = await Token.findOne({ where: { user_id: user_id } });
+        const token = userToken.token;
+        const fakeToken = generateToken(user_id);
+        await Token.update(
+            { token: fakeToken },
+            { where: { user_id: user_id } },
+            { transaction }
+        );
+        await Token.update(
+            { token: token },
+            { where: { user_id: user_id } },
+            { transaction }
+        );
+    } catch (err) {
+        throw Error(exception.SERVER_ERROR);
+    }
+};
 
 const getUserId = async (token) => {
-       try {
-           const userId = await getUserIdByToken(token);
-           return userId;
-       } catch (error) {
-           throw Error(exception.INVALID_TOKEN);
-       }
-    };
-    
+    try {
+        const userId = await getUserIdByToken(token);
+        return userId;
+    } catch (error) {
+        throw Error(exception.INVALID_TOKEN);
+    }
+};
+
 const tokenMiddleware = async (req, res, next) => {
     // read the token from header
     const token = req.headers['x-access-token'];
@@ -60,13 +73,13 @@ const tokenMiddleware = async (req, res, next) => {
     if (!token) {
         next(createError(sc.BAD_REQUEST));
     }
-  
+
     try {
         const userId = await getUserId(token);
         req.userId = userId;
         await refreshTokenTimeOfUser(userId);
     } catch (error) {
-        switch(error.message) {
+        switch (error.message) {
             case exception.EXPIRED_TOKEN:
             case exception.INVALID_TOKEN:
                 next(createError(sc.INVALID_ACCOUNT, 'Token Error'));

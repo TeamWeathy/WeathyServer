d399cc1e33b6b40a736a7874d96846b193d52d44
yxxshin <samshin3910@snu.ac.kr>
2021-02-24 03:11:26 +0900

    Implemented clothesService Test Codes

--- WeathyServer/tests/modules/tokenMiddleware.spec.js
+++ WeathyServer/tests/modules/tokenMiddleware.spec.js
@@ -8,7 +8,7 @@ describe('tokenMiddleware test', function () {
     describe('validateToken test', () => {
         // testing tokens::
         // userID 22 (Should be Updated)
-        // userID 21 (Should not be Updated)
+        // userID 25 (Should not be Updated)
 
         const firstTestUserId = 22;
         const secondTestUserId = 25;
